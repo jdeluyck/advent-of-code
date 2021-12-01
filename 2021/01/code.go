3d2ba75699de022a60623b79be98acf14fd7e45d
jdeluyck <5451787+jdeluyck@users.noreply.github.com>
2021-12-01 19:48:20 +0100

    2021/01

---
+++ 2021/01/code.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func parseArgs() (inputFile string) {
+	fmt.Print(os.Args)
+
+	if len(os.Args) == 2 {
+		inputFile = os.Args[1]
+	} else {
+		log.Fatalf("Usage: %v INPUTFILE", os.Args[0])
+	}
+
+	return
+}
+
+func main() {
+	inputFile := parseArgs()
+	fh, err := os.Open(inputFile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileScanner := bufio.NewScanner(fh)
+
+	var vals []int
+
+	for fileScanner.Scan() {
+		val, _ := strconv.Atoi(fileScanner.Text())
+		vals = append(vals, val)
+	}
+
+	fmt.Printf("Part 1: %v measurements are larger\n", part1(vals))
+	fmt.Printf("Part 2: %v measurements are larger\n", part2(vals))
+}
+
+func part2(vals []int) (depthIncrease int) {
+	depthIncrease = 0
+
+	var windows []int
+
+	for idx := 1; idx < len(vals)-1; idx++ {
+		windows = append(windows, vals[idx-1]+vals[idx]+vals[idx+1])
+	}
+
+	depthIncrease = part1(windows)
+
+	return
+}
+
+func part1(vals []int) (depthIncrease int) {
+	depthIncrease = 0
+
+	prevVal := 0
+
+	for _, val := range vals {
+		if prevVal == 0 {
+			fmt.Printf("%v (N/A)\n", val)
+		} else {
+			if val == prevVal {
+				fmt.Printf("%v (no change)\n", val)
+			} else if val > prevVal {
+				fmt.Printf("%v (increased)\n", val)
+				depthIncrease += 1
+			} else {
+				fmt.Printf("%v (decreased)\n", val)
+			}
+		}
+		prevVal = val
+	}
+
+	return
+}
